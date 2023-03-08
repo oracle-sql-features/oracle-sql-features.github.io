86726fdc3529e4f0ca171609a2d07e86fe35b1de
Andres Almiray <aalmiray@gmail.com>
2023-03-08 17:31:21 +0100

    Find nested feature pages

--- .github/scripts/generate-navigation.sh
+++ .github/scripts/generate-navigation.sh
@@ -10,12 +10,13 @@ echo "* xref:index.adoc[]" > "${CATEGORIES}/nav.adoc"
 echo "* xref:index.adoc[]" > "${VERSIONS}/nav.adoc"
 mkdir -p "${PWD}/docs/modules/features/partials"
 
-for page in $(ls ${FEATURES})
+for FEATURE in $(find "${FEATURES}" -name "*.adoc" -print)
 do
-    echo "➡️  Processing ${page}"
-    cp "${FEATURES}/${page}" "${PWD}/docs/modules/features/partials/${page}"
-    DATABASE_CATEGORIES=$(grep ":database-category:" "${FEATURES}/${page}")
-    DATABASE_VERSION=$(grep ":database-version:" "${FEATURES}/${page}" | awk '{print $2}')
+    PAGE=$(basename "${FEATURE}")
+    echo "➡️  Processing ${PAGE}"
+    cp "${FEATURE}" "${PWD}/docs/modules/features/partials/${PAGE}"
+    DATABASE_CATEGORIES=$(grep ":database-category:" "${FEATURES}/${PAGE}")
+    DATABASE_VERSION=$(grep ":database-version:" "${FEATURES}/${PAGE}" | awk '{print $2}')
 
     IFS=\  read -a CATEGORIES_ARRAY <<<"$DATABASE_CATEGORIES"
     for DATABASE_CATEGORY in "${CATEGORIES_ARRAY[@]:1}"; do
@@ -33,7 +34,7 @@ do
         fi
 
         # create pages
-        echo "include::features:partial\$${page}[]" > "${CATEGORIES}/pages/${DATABASE_CATEGORY}/${page}"
+        echo "include::features:partial\$${PAGE}[]" > "${CATEGORIES}/pages/${DATABASE_CATEGORY}/${PAGE}"
 
         # update navs
         CATEGORY_EXISTS=$(grep "${DATABASE_CATEGORY}/index.adoc" "${CATEGORIES}/nav.adoc")
@@ -42,7 +43,7 @@ do
         then
           echo "** xref:${DATABASE_CATEGORY}/index.adoc[]" >> "${CATEGORIES}/nav.adoc"
         fi
-        echo "*** xref:${DATABASE_CATEGORY}/${page}[]" >> "${CATEGORIES}/nav.adoc"
+        echo "*** xref:${DATABASE_CATEGORY}/${PAGE}[]" >> "${CATEGORIES}/nav.adoc"
     done
 
     echo "DATABASE_VERSION = $DATABASE_VERSION"
@@ -57,7 +58,7 @@ do
     fi
 
     # create pages
-    echo "include::features:partial\$${page}[]" > "${VERSIONS}/pages/${DATABASE_VERSION}/${page}"
+    echo "include::features:partial\$${PAGE}[]" > "${VERSIONS}/pages/${DATABASE_VERSION}/${PAGE}"
 
     # update navs
     VERSION_EXISTS=$(grep "${DATABASE_VERSION}/index.adoc" "${VERSIONS}/nav.adoc")
@@ -66,7 +67,7 @@ do
     then
       echo "** xref:${DATABASE_VERSION}/index.adoc[]" >> "${VERSIONS}/nav.adoc"
     fi
-    echo "*** xref:${DATABASE_VERSION}/${page}[]" >> "${VERSIONS}/nav.adoc"
+    echo "*** xref:${DATABASE_VERSION}/${PAGE}[]" >> "${VERSIONS}/nav.adoc"
 done
 
 if [ -n "${GIT_COMMIT}" ];
