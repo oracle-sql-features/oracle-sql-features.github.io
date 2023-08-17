560a4f85d8d3004d775f0c62574818b9dc3a6e91
Andres Almiray <aalmiray@gmail.com>
2023-08-17 20:16:48 +0200

    Delete old navigation script

--- .github/scripts/generate_navigation.java
+++ .github/scripts/generate_navigation.java
@@ -8,10 +8,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -22,6 +23,7 @@ import java.util.stream.Stream;
 import static java.lang.System.lineSeparator;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.FileVisitResult.CONTINUE;
+import static java.util.stream.Collectors.groupingBy;
 
 public class generate_navigation {
     public static void main(String... args) throws Exception {
@@ -39,22 +41,32 @@ public class generate_navigation {
         var categoriesMap = new TreeMap<String, Set<Feature>>();
         var versionsMap = new TreeMap<String, Set<Feature>>();
 
-        FeatureFinder featureFinder = new FeatureFinder();
-        Files.walkFileTree(featuresPath, featureFinder);
+        SourceFinder sourceFinder = new SourceFinder();
+        Files.walkFileTree(featuresPath, sourceFinder);
 
-        if (!featureFinder.success) {
+        if (!sourceFinder.success) {
             System.err.println("❌ Unexpected error occurred while searching for feature files");
             System.exit(1);
         }
 
-        if (!featureFinder.duplicates.isEmpty()) {
+        if (!sourceFinder.duplicates.isEmpty()) {
             System.err.println("❌ Found duplicate feature files. Filenames must be unique. Rename and retry");
-            featureFinder.duplicates.forEach(System.err::println);
+            sourceFinder.duplicates.forEach(System.err::println);
             System.exit(1);
         }
 
+        // copy categories & versions
+        System.out.printf("🛠  Generating categories%n");
+        copyPartials(sourceFinder.categories.values(), "Categories", categoriesPath);
+        System.out.printf("🛠  Generating versions%n");
+        copyPartials(sourceFinder.versions.values(), "Versions", versionsPath);
+
+        // generate include matrix for categories x versions and vice-versa
+        generateIncludes(categoriesPath, sourceFinder.categories, sourceFinder.versions, "categories", "versions");
+        generateIncludes(versionsPath, sourceFinder.versions, sourceFinder.categories, "versions", "categories");
+
         var features = new LinkedHashSet<Feature>();
-        for (Map.Entry<String, Path> e : featureFinder.features.entrySet()) {
+        for (Map.Entry<String, Path> e : sourceFinder.features.entrySet()) {
             var page = e.getKey();
             var f = e.getValue();
 
@@ -72,42 +84,150 @@ public class generate_navigation {
         }
 
         System.out.printf("🛠  Generating pages%n");
-        generateNavigation(categoriesPath, categoriesMap);
-        generateNavigation(versionsPath, versionsMap);
+        generateNavigation(categoriesPath, categoriesMap, true);
+        generateNavigation(versionsPath, versionsMap, false);
         copyFeatures(features, partialsPath);
     }
 
-    private static void generateNavigation(Path path, Map<String, Set<Feature>> data) throws IOException {
-        StringBuilder b = new StringBuilder("* xref:index.adoc[]");
-        b.append(lineSeparator());
+    private static void generateIncludes(Path path, Map<String, ? extends Item> outer, Map<String, ? extends Item> inner,
+                                         String outerLabel, String innerLabel) throws IOException {
+        var pages = path.resolve("pages");
+        Files.createDirectories(pages);
+
+        for (Item o : outer.values()) {
+            var index = pages.resolve(o.getId()).resolve("index.adoc");
+            Files.createDirectories(index.getParent());
+            Files.write(index, ("include::" + outerLabel + ":partial$" + o.getId() + ".adoc[]" + lineSeparator()).getBytes(UTF_8));
+            for (Item i : inner.values()) {
+                index = pages.resolve(o.getId()).resolve(i.getId()).resolve("index.adoc");
+                Files.createDirectories(index.getParent());
+                Files.write(index, ("include::" + innerLabel + ":partial$" + i.getId() + ".adoc[]" + lineSeparator()).getBytes(UTF_8));
+            }
+        }
+    }
+
+    private static void generateNavigation(Path path, Map<String, Set<Feature>> data, boolean versions) throws IOException {
+        StringBuilder indexNav = new StringBuilder("* xref:index.adoc[]");
+        indexNav.append(lineSeparator());
 
         for (Map.Entry<String, Set<Feature>> e : data.entrySet()) {
-            var k = e.getKey();
-            var v = e.getValue();
+            var classifier = e.getKey();
+            var features = e.getValue();
 
-            System.out.printf("🔖 Generating %s%n", k);
-            var index = path.resolve("pages/" + k + "/index.adoc");
+            System.out.printf("🔖 Generating %s%n", classifier);
+            var index = path.resolve("pages/" + classifier + "/index.adoc");
             // create page if it does not exist
             if (!Files.exists(index)) {
                 Files.createDirectories(index.getParent());
-                Files.write(index, ("= " + k + lineSeparator()).getBytes(UTF_8));
+                Files.write(index, ("= " + classifier + lineSeparator()).getBytes(UTF_8));
             }
 
-            b.append("** xref:" + k + "/index.adoc[]")
+            indexNav.append("** xref:")
+                .append(classifier)
+                .append("/index.adoc[]")
                 .append(lineSeparator());
-            for (Feature feature: v) {
-                var f = feature.path.getFileName().toString();
 
-                b.append("*** xref:" + k + "/" + f + "[]")
+            System.out.printf("🔖 Generating %s/features%n", classifier);
+
+            indexNav.append("*** All")
+                .append(lineSeparator());
+
+            for (Feature feature : features) {
+                var featureFilename = feature.path.getFileName().toString();
+
+                indexNav.append("**** xref:")
+                    .append(classifier)
+                    .append("/features/")
+                    .append(featureFilename)
+                    .append("[]")
                     .append(lineSeparator());
 
                 // create partial
-                System.out.printf("📝 Generating %s/%s%n", k, f);
-                var partial = path.resolve("pages/" + k + "/" + f);
-                Files.write(partial, ("include::features:partial$" + f + "[]" + lineSeparator()).getBytes(UTF_8));
+                System.out.printf("📝 Generating %s/features/%s%n", classifier, featureFilename);
+                var partial = path.resolve("pages/" + classifier + "/features/" + featureFilename);
+                Files.createDirectories(partial.getParent());
+                Files.write(partial, ("include::features:partial$" + featureFilename + "[]" + lineSeparator()).getBytes(UTF_8));
+            }
+
+            if (versions) {
+                // versions
+                Map<String, List<Feature>> versionedFeatures = features.stream()
+                    .collect(groupingBy(Feature::getVersion));
+
+                for (Map.Entry<String, List<Feature>> ve : versionedFeatures.entrySet()) {
+                    String version = ve.getKey();
+
+                    System.out.printf("🔖 Generating %s/%s%n", classifier, version);
+
+                    indexNav.append("*** xref:")
+                        .append(classifier)
+                        .append("/")
+                        .append(version)
+                        .append("/index.adoc[]")
+                        .append(lineSeparator());
+
+                    for (Feature feature : ve.getValue()) {
+                        var featureFilename = feature.path.getFileName().toString();
+
+                        indexNav.append("**** xref:").
+                            append(classifier)
+                            .append("/")
+                            .append(version)
+                            .append("/")
+                            .append(featureFilename)
+                            .append("[]")
+                            .append(lineSeparator());
+
+                        // create partial
+                        System.out.printf("📝 Generating %s/%s/%s%n", classifier, version, featureFilename);
+                        var partial = path.resolve("pages/" + classifier + "/" + version + "/" + featureFilename);
+                        Files.createDirectories(partial.getParent());
+                        Files.write(partial, ("include::features:partial$" + featureFilename + "[]" + lineSeparator()).getBytes(UTF_8));
+                    }
+                }
+            } else {
+                var categorized = new TreeMap<String, Set<Feature>>();
+                for (Feature feature : features) {
+                    for (String category : feature.categories) {
+                        categorized.computeIfAbsent(category, k -> new TreeSet<>())
+                            .add(feature);
+                    }
+                }
+
+                for (Map.Entry<String, Set<Feature>> k : categorized.entrySet()) {
+                    var category = k.getKey();
+
+                    System.out.printf("🔖 Generating %s/%s%n", classifier, category);
+
+                    indexNav.append("*** xref:")
+                        .append(classifier)
+                        .append("/")
+                        .append(category)
+                        .append("/index.adoc[]")
+                        .append(lineSeparator());
+
+                    for (Feature feature : k.getValue()) {
+                        var featureFilename = feature.path.getFileName().toString();
+
+                        indexNav.append("**** xref:").
+                            append(classifier)
+                            .append("/")
+                            .append(category)
+                            .append("/")
+                            .append(featureFilename)
+                            .append("[]")
+                            .append(lineSeparator());
+
+                        // create partial
+                        System.out.printf("📝 Generating %s/%s/%s%n", classifier, category, featureFilename);
+                        var partial = path.resolve("pages/" + classifier + "/" + category + "/" + featureFilename);
+                        Files.createDirectories(partial.getParent());
+                        Files.write(partial, ("include::features:partial$" + featureFilename + "[]" + lineSeparator()).getBytes(UTF_8));
+                    }
+                }
             }
         }
-        Files.write(path.resolve("nav.adoc"), b.toString().getBytes(UTF_8));
+        Files.write(path.resolve("nav.adoc"), indexNav.toString().getBytes(UTF_8));
     }
 
     private static void copyFeatures(Set<Feature> features, Path partials) throws IOException {
@@ -119,6 +239,33 @@ public class generate_navigation {
             content = content.replace("[[feature_summary]]", feature.asSummary());
             Files.write(partials.resolve(feature.path.getFileName()), content.getBytes(UTF_8));
         }
+    }
+
+    private static <I extends Item> void copyPartials(Collection<I> items, String title, Path path) throws IOException {
+        var partials = path.resolve("partials");
+        var pages = path.resolve("pages");
+
+        deleteFiles(path);
+        Files.createDirectories(partials);
+        Files.createDirectories(pages);
+
+        for (I item : items) {
+            var content = new String(Files.readAllBytes(item.getPath()));
+            Files.write(partials.resolve(item.getPath().getFileName()), content.getBytes(UTF_8));
+        }
+
+        var index = pages.resolve("index.adoc");
+        var content = new StringBuilder("= ")
+            .append(title)
+            .append(lineSeparator())
+            .append(lineSeparator());
+        for (Item item : items) {
+            content.append("* xref:")
+                .append(item.getId())
+                .append("/index.adoc[]")
+                .append(lineSeparator());
+        }
+        Files.write(index, content.toString().getBytes(UTF_8));
     }
 
     private static String extractAttribute(Path file, String attributeName) throws IOException {
@@ -134,18 +281,20 @@ public class generate_navigation {
         return line.get().substring(attributeName.length() + 1).trim();
     }
 
-    private static String extractTitle(Path file) throws IOException {
-        Optional<String> line = Files.lines(file)
-            .map(String::trim)
-            .filter(s -> s.startsWith("= "))
-            .findFirst();
+    private static String extractTitle(Path file) {
+        try (Stream<String> stream = Files.lines(file)) {
+            Optional<String> line = stream.map(String::trim)
+                .filter(s -> s.startsWith("= "))
+                .findFirst();
+            if (line.isEmpty()) {
+                System.err.printf("❌ Missing title in %s%n", file.toAbsolutePath());
+                System.exit(1);
+            }
 
-        if (line.isEmpty()) {
-            System.err.printf("❌ Missing title in %s%n", file.toAbsolutePath());
-            System.exit(1);
+            return line.get().substring(2).trim();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
         }
-
-        return line.get().substring(2).trim();
     }
 
     private static void deleteFiles(Path path) throws IOException {
@@ -160,7 +309,9 @@ public class generate_navigation {
         }
     }
 
-    private static class FeatureFinder implements FileVisitor<Path> {
+    private static class SourceFinder implements FileVisitor<Path> {
+        private final Map<String, Category> categories = new TreeMap<>();
+        private final Map<String, Version> versions = new TreeMap<>();
         private final Map<String, Path> features = new LinkedHashMap<>();
         private final Set<Path> duplicates = new LinkedHashSet<>();
         private boolean success = true;
@@ -174,7 +325,14 @@ public class generate_navigation {
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
             String filename = file.getFileName().toString();
             if (filename.endsWith(".adoc")) {
-                if (!features.containsKey(filename)) {
+                if (file.getParent().getFileName().toString().equals("_categories")) {
+                    String id = filename.substring(0, filename.length() - 5);
+                    Category category = new Category(file, id);
+                    categories.put(category.getTitle(), category);
+                } else if (file.getParent().getFileName().toString().equals("_versions")) {
+                    String version = filename.substring(0, filename.length() - 5);
+                    versions.put(version, new Version(file, version));
+                } else if (!features.containsKey(filename)) {
                     features.put(filename, file);
                 } else {
                     duplicates.add(file);
@@ -195,17 +353,48 @@ public class generate_navigation {
         }
     }
 
-    private static class Feature implements Comparable<Feature> {
+    public interface Item {
+        String getId();
+
+        String getTitle();
+
+        Path getPath();
+    }
+
+    private static class Feature implements Comparable<Feature>, Item {
         private final Path path;
+        private final String id;
         private final String version;
         private final String[] categories;
         private final String title;
 
-        private Feature(Path path, String version, String[] categories) throws IOException {
+        private Feature(Path path, String version, String[] categories) {
             this.path = path;
             this.version = version;
             this.categories = categories;
             this.title = extractTitle(path);
+            var id = path.getFileName().toString();
+            this.id = id.substring(0, id.length() - 5);
+        }
+
+
+        @Override
+        public String getId() {
+            return id;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public Path getPath() {
+            return path;
+        }
+
+        public String getVersion() {
+            return version;
         }
 
         private String asSummary() {
@@ -235,15 +424,73 @@ public class generate_navigation {
         public int compareTo(Feature o) {
             return title.compareTo(o.title);
         }
+    }
+
+    private static class Category implements Comparable<Category>, Item {
+        private final Path path;
+        private final String id;
+        private final String title;
+
+        private Category(Path path, String id) {
+            this.path = path;
+            this.id = id;
+            this.title = extractTitle(path);
+        }
 
         @Override
-        public String toString() {
-            return "Feature{" +
-                "path=" + path +
-                ", version='" + version + '\'' +
-                ", title='" + title + '\'' +
-                ", categories=" + Arrays.toString(categories) +
-                '}';
+        public Path getPath() {
+            return path;
+        }
+
+        @Override
+        public String getId() {
+            return id;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public int compareTo(Category o) {
+            return title.compareTo(o.title);
+        }
+    }
+
+    private static class Version implements Comparable<Version>, Item {
+        private final Path path;
+        private final String version;
+        private final String title;
+
+        private Version(Path path, String version) {
+            this.path = path;
+            this.version = version;
+            this.title = extractTitle(path);
+        }
+
+        @Override
+        public String getId() {
+            return version;
+        }
+
+        @Override
+        public Path getPath() {
+            return path;
+        }
+
+        public String getVersion() {
+            return version;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public int compareTo(Version o) {
+            return title.compareTo(o.title);
         }
     }
 }
