8355104f52b6f051db0dad25b75a3f712c8c8dde
Andres Almiray <aalmiray@gmail.com>
2024-08-14 14:17:27 +0200

    fix: Install dependencies before generating the site

--- generate.sh
+++ generate.sh
@@ -4,6 +4,11 @@ echo "🔄 Generating navigation"
 java .github/scripts/generate_navigation.java "$(pwd)"
 
 echo ""
+echo "📦 Installing dependencies"
+echo "Please wait..."
+npm install
+
+echo ""
 echo "🛠️  Building site"
 echo "Please wait..."
 npx antora local-playbook.yml
